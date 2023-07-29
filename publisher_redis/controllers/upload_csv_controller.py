b011f228f6d2d1cc1a3b8f5c9c4ec156d1fce1dc
manueltobon <manuelpope@gmail.com>
2023-07-29 12:27:52 -0300

    cleaning minio

--- publisher_redis/controllers/upload_csv_controller.py
+++ publisher_redis/controllers/upload_csv_controller.py
@@ -1,29 +1,30 @@
 import os
+from fastapi import HTTPException, status
 from fastapi import APIRouter, File, UploadFile
 from minio.error import S3Error
 from buckets.bucket_service import bucket
+import logging
 
 router = APIRouter()
 
-
-
+logger = logging.getLogger(__name__)
 @router.post("/upload_csv/")
 async def upload_csv(file: UploadFile = File(...)):
     # Verificar que el archivo subido sea un CSV
     if not file.filename.endswith(".csv"):
-        return {"error": "El archivo debe ser un archivo CSV."}
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="El archivo debe ser un archivo CSV.")
 
     # Leer el contenido del archivo CSV
-    content = await file.read()
 
     # Subir el archivo al bucket de MinIO
     try:
         file_path = os.path.join(bucket.get_bucket_name(), file.filename)
-        bucket.get_client().put_object(bucket.get_bucket_name(), file.filename, content, len(content))
+        bucket.get_client().put_object(bucket.get_bucket_name(), file.filename, file.file, file.size)
     except S3Error as e:
-        return {"error": f"Error al subir el archivo a MinIO: {e}"}
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                            detail=f"Error al subir el archivo a MinIO: {e}")
 
     # Llamar a la función para procesar el mensaje (entrenamiento del modelo)
-    print(file.filename)
+    logger.info(file.filename)
 
     return {"message": "Archivo subido y procesado correctamente."}
