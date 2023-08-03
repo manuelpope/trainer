04a04b7bbce825aeb69340aa3ef53e6c6e832a43
manuelpope <manuelpope@gmail.com>
2023-08-03 17:15:04 -0300

    changes for publisher

--- publisher_redis/controllers/get_file_controller.py
+++ publisher_redis/controllers/get_file_controller.py
@@ -5,12 +5,13 @@ from buckets.bucket_service import bucket
 router = APIRouter()
 
 
-@router.get("/get_file/")
+@router.get("/get_file/{filename}")
 async def get_file(filename: str, response: Response):
     # Obtener el contenido del archivo desde MinIO y retornarlo en la respuesta
     try:
         file_stream = bucket.get_client().get_object(settings.bucket_name, filename)
+        file_content = file_stream.read()
         response.headers["Content-Disposition"] = f"attachment; filename={filename}"
-        return Response(content=file_stream, media_type="application/octet-stream")
+        return Response(content=file_content, media_type="application/octet-stream")
     except S3Error as e:
         return {"error": f"Error al obtener el archivo desde MinIO: {e}"}
